f6b6f2b9bd61e62067cc7b01952bef3b6a3803be
OMAR-Zizo827 <omar.abdos2005@gmail.com>
2025-12-17 18:11:18 +0200

    okay

--- yasta-focus-backend/app.js
+++ yasta-focus-backend/app.js
@@ -25,7 +25,7 @@ import adminRouter from './routes/adminRouter.js';
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  origin: ['http://localhost:5173', 'http://localhost:5174',process.env.FRONTEND_URL],
   credentials: true,
 }));
 app.use(express.json());
